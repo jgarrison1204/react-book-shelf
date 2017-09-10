217d56482f2c30d1fa90fab9a71433734c53ff6b
Jacob Garrison <jgarrison1204@gmail.com>
2017-09-10 09:57:44 -0700

    Components folder created, SelectShelf component created and refacted in App.js...need to add functionality

---
+++ src/components/SelectShelf.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SelectShelf() {
+	return (
+		<div className="book-shelf-changer">
+			<select>
+				<option value="none" disabled>Move to...</option>
+				<option value="currentlyReading">Currently Reading</option>
+				<option value="wantToRead">Want to Read</option>
+				<option value="read">Read</option>
+				<option value="none">None</option>
+			</select>
+		</div>
+	)
+}
+
+export default SelectShelf
